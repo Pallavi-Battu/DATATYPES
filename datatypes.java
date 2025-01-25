18b290a5d03449b7445b1146210f123f2ee178f5
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-25 12:33:58 +0530

    Update datatypes.java

--- datatypes.java
+++ datatypes.java
@@ -1,23 +1,19 @@
 
 public class datatypes{
     public static void main(String[] args) {
-        // Integer types
-        byte byteValue = 100; // 1 byte
-        short shortValue = 10000; // 2 bytes
-        int intValue = 100000; // 4 bytes
-        long longValue = 100000L; // 8 bytes
+        
+        byte byteValue = 100; 
+        short shortValue = 10000; 
+        int intValue = 100000;
+        long longValue = 100000L; 
 
-        // Floating-point types
-        float floatValue = 5.75f; // 4 bytes
-        double doubleValue = 19.99; // 8 bytes
+        float floatValue = 5.75f; 
+        double doubleValue = 19.99; 
+        char charValue = 'A'; 
+        
+        boolean booleanValue = true; 
 
-        // Character type
-        char charValue = 'A'; // 2 bytes
-
-        // Boolean type
-        boolean booleanValue = true; // 1 bit
-
-        // Displaying the values
+        
         System.out.println("Byte Value: " + byteValue);
         System.out.println("Short Value: " + shortValue);
         System.out.println("Integer Value: " + intValue);
